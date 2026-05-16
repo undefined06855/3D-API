40508f571dd0bc945a515eef98cdcccd60e5ff45
undefined06855 <currentcashew57@gmail.com>
2026-05-16 20:47:22 +0100

    actually actually fix inline definitions for ios

--- src/inline-defs.cpp
+++ src/inline-defs.cpp
@@ -2,16 +2,110 @@
 
 #ifdef GEODE_IS_IOS
 
+kmMat4* kmMat4RotationX(kmMat4* pOut, const kmScalar radians) {
+	/*
+		 |  1  0       0       0 |
+	 M = |  0  cos(A) -sin(A)  0 |
+	     |  0  sin(A)  cos(A)  0 |
+	     |  0  0       0       1 |
+
+	*/
+
+	pOut->mat[0] = 1.0f;
+	pOut->mat[1] = 0.0f;
+	pOut->mat[2] = 0.0f;
+	pOut->mat[3] = 0.0f;
+
+	pOut->mat[4] = 0.0f;
+	pOut->mat[5] = cosf(radians);
+	pOut->mat[6] = sinf(radians);
+	pOut->mat[7] = 0.0f;
+
+	pOut->mat[8] = 0.0f;
+	pOut->mat[9] = -sinf(radians);
+	pOut->mat[10] = cosf(radians);
+	pOut->mat[11] = 0.0f;
+
+	pOut->mat[12] = 0.0f;
+	pOut->mat[13] = 0.0f;
+	pOut->mat[14] = 0.0f;
+	pOut->mat[15] = 1.0f;
+
+	return pOut;
+}
+
+kmMat4* kmMat4RotationY(kmMat4* pOut, const kmScalar radians) {
+	/*
+	     |  cos(A)  0   sin(A)  0 |
+	 M = |  0       1   0       0 |
+	     | -sin(A)  0   cos(A)  0 |
+	     |  0       0   0       1 |
+	*/
+
+	pOut->mat[0] = cosf(radians);
+	pOut->mat[1] = 0.0f;
+	pOut->mat[2] = -sinf(radians);
+	pOut->mat[3] = 0.0f;
+
+	pOut->mat[4] = 0.0f;
+	pOut->mat[5] = 1.0f;
+	pOut->mat[6] = 0.0f;
+	pOut->mat[7] = 0.0f;
+
+	pOut->mat[8] = sinf(radians);
+	pOut->mat[9] = 0.0f;
+	pOut->mat[10] = cosf(radians);
+	pOut->mat[11] = 0.0f;
+
+	pOut->mat[12] = 0.0f;
+	pOut->mat[13] = 0.0f;
+	pOut->mat[14] = 0.0f;
+	pOut->mat[15] = 1.0f;
+
+	return pOut;
+}
+
+kmMat4* kmMat4RotationZ(kmMat4* pOut, const kmScalar radians) {
+	/*
+	     |  cos(A)  -sin(A)   0   0 |
+	 M = |  sin(A)   cos(A)   0   0 |
+	     |  0        0        1   0 |
+	     |  0        0        0   1 |
+	*/
+
+	pOut->mat[0] = cosf(radians);
+	pOut->mat[1] = sinf(radians);
+	pOut->mat[2] = 0.0f;
+	pOut->mat[3] = 0.0f;
+
+	pOut->mat[4] = -sinf(radians);
+	pOut->mat[5] = cosf(radians);
+	pOut->mat[6] = 0.0f;
+	pOut->mat[7] = 0.0f;
+
+	pOut->mat[8] = 0.0f;
+	pOut->mat[9] = 0.0f;
+	pOut->mat[10] = 1.0f;
+	pOut->mat[11] = 0.0f;
+
+	pOut->mat[12] = 0.0f;
+	pOut->mat[13] = 0.0f;
+	pOut->mat[14] = 0.0f;
+	pOut->mat[15] = 1.0f;
+
+	return pOut;
+}
+
 kmMat4* const unlinked::kmMat4RotationPitchYawRoll(kmMat4* pOut, const kmScalar pitch, const kmScalar yaw, const kmScalar roll) {
     kmMat4 yaw_matrix;
     kmMat4 roll_matrix;
     kmMat4 pitch_matrix;
 
-    kmMat4RotationY(&yaw_matrix, yaw);
+    unlinked::kmMat4RotationY(&yaw_matrix, yaw);
 
-    kmMat4RotationX(&pitch_matrix, pitch);
+    unlinked::kmMat4RotationX(&pitch_matrix, pitch);
 
-    kmMat4RotationZ(&roll_matrix, roll);
+    unlinked::kmMat4RotationZ(&roll_matrix, roll);
 
     kmMat4Multiply(pOut, &pitch_matrix, &roll_matrix);
     kmMat4Multiply(pOut, &yaw_matrix, pOut);
